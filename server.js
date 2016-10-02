3f40adb3d7b0d858948efc52e2263303b214b143
Awanish Pandey <awanishpandey86@gmail.com>
2016-10-02 15:04:42 +0530

    Convert GET /todos/:id to use sequelize

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ var todoNextId = 1;
 
 app.use(bodyParser.json());
 
+
 // GET /todos?completed=true&q=work
 
 app.get('/todos', function (req, res) {
@@ -37,20 +38,22 @@ app.get('/todos', function (req, res) {
 	res.json(filteredTodos);
 });
 
+// GET /todos/:id
+
 app.get('/todos/:id', function (req, res) {
 
 	var todoId = parseInt(req.params.id);
 
-	var mathedTodo = _.findWhere(todos,{id: todoId});
-
-	if (mathedTodo) {
-		res.json(mathedTodo);
-	} else {
-		res.status(404).send();
-	}
-
-
-	//res.send('Asking for todo with id of ' + req.params.id);
+	db.todo.findById(todoId).then(function (todo){
+		if (!!todo) {
+			res.json(todo.toJSON());
+		} else {
+			res.status(404).send();
+		}
+	}, function (e) {
+		res.status(500).send();
+	});
+	
 });
 
 
@@ -66,24 +69,11 @@ app.post('/todos',function(req, res) {
 var body = _.pick(req.body,'description','completed');
 var todo = db.todo;
 
-todo.create(body).then(function (todo) {
-	res.json(todo.toJSON());
-}, function (e) {
-	res.status(400).json(e);
-});
-
-
-
-// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-// 	return res.status(400).send();
-// }
-
-// body.id = todoNextId++;
-// body.description = body.description.trim();
-
-// todos.push(body);
-
-// res.json(body);
+	todo.create(body).then(function (todo) {
+		res.json(todo.toJSON());
+	}, function (e) {
+		res.status(400).json(e);
+	});
 
 });
 
